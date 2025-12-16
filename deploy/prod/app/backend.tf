a5c94e5b8f2297390fdc30f44ae1d0ae3b2c38e6
Andrew Howden <hello@andrewhowden.com>
2025-12-16 01:35:35 +0100

    fix: configure backend to impersonate service account

--- deploy/prod/app/backend.tf
+++ deploy/prod/app/backend.tf
@@ -1,6 +1,6 @@
-terraform {
   backend "gcs" {
-    bucket = "andrewhowdencom-infrastructure-state"
-    prefix = "workctl-app"
+    bucket                      = "andrewhowdencom-infrastructure-state"
+    prefix                      = "workctl-app"
+    impersonate_service_account = "workctl-gh-actions@andrewhowdencom.iam.gserviceaccount.com"
   }
 }
