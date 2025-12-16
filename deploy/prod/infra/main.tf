e6b9aa1bef15c5960cf2eceea94a803f7bffc9a9
Andrew Howden <hello@andrewhowden.com>
2025-12-16 01:15:01 +0100

    feat: add workctl-gh-actions service account

--- deploy/prod/infra/main.tf
+++ deploy/prod/infra/main.tf
@@ -29,3 +29,35 @@ import {
   id = "projects/andrewhowdencom/managedZones/workctl-zone"
   to = google_dns_managed_zone.workctl
 }
+
+# Terraform Executor Service Account
+resource "google_service_account" "workctl_gh_actions" {
+  account_id   = "workctl-gh-actions"
+  display_name = "Workctl GitHub Actions Executor"
+  description  = "Service Account for GitHub Actions to execute Terraform"
+}
+
+# Project-level permissions
+resource "google_project_iam_member" "workctl_gh_actions_project_roles" {
+  for_each = toset([
+    "roles/run.admin",
+    "roles/storage.admin",
+  ])
+  project = "andrewhowdencom"
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.workctl_gh_actions.email}"
+}
+
+# Allow impersonating/using the workctl Service Account
+resource "google_service_account_iam_member" "workctl_gh_actions_impersonate_workctl" {
+  service_account_id = google_service_account.workctl.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${google_service_account.workctl_gh_actions.email}"
+}
+
+# Allow managing DNS records in the workctl zone
+resource "google_dns_managed_zone_iam_member" "workctl_gh_actions_dns_admin" {
+  managed_zone = google_dns_managed_zone.workctl.name
+  role         = "roles/dns.admin"
+  member       = "serviceAccount:${google_service_account.workctl_gh_actions.email}"
+}
